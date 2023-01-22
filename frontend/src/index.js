59e390c0cfb3fcc5c460bcb450b3584a3c614215
uditkatyal <76552469+uditkatyal@users.noreply.github.com>
2023-01-22 21:32:53 +0530

    work

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,6 +14,7 @@ root.render(
     <App />
   </Provider>
 );
+// some comment
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
